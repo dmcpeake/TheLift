b651388684f57f48b0591f8832e722fa76fbaacd
dmcpeake <dan@the-lift.org>
2025-10-08 12:03:21 +0100

    Move indicator line down 1px more (from -21px to -22px)

--- src/components/checkin/CheckInHome.tsx
+++ src/components/checkin/CheckInHome.tsx
@@ -733,7 +733,7 @@ export function CheckInHome() {
               {location.pathname === '/checkin/garden' && (
                 <div style={{
                   position: 'absolute',
-                  bottom: '-21px',
+                  bottom: '-22px',
                   left: 0,
                   right: 0,
                   height: '2px',
@@ -761,7 +761,7 @@ export function CheckInHome() {
               {location.pathname === '/checkin/home' && (
                 <div style={{
                   position: 'absolute',
-                  bottom: '-21px',
+                  bottom: '-22px',
                   left: 0,
                   right: 0,
                   height: '2px',
